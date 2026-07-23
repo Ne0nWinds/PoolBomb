8389408e45c814273dd3f252852a5f081c4a4e54
Caden Parker <cadenparker@ymail.com>
2026-07-22 19:27:02 -0500

    Basic Local Multiplayer

--- compile.ps1
+++ compile.ps1
@@ -2,65 +2,105 @@ param(
 	[switch]$optimize,
 	[switch]$debuginfo,
 	[switch]$win32,
-	[switch]$web
+	[switch]$web,
+	[switch]$watch
 )
 
 
-if ($win32) {
-	$cf = @(
-		"-std=c99",
-		"-Wall",
-		"-Wextra",
-		"-Wno-unused-parameter",
-		"-Wno-unused-function",
-		"-pedantic-errors",
-		"-DPLATFORM_WIN32",
-		"-Ilib/"
-	)
+function Invoke-Build {
+	if ($win32) {
+		$cf = @(
+			"-std=c99",
+			"-Wall",
+			"-Wextra",
+			"-Wno-unused-parameter",
+			"-Wno-unused-function",
+			"-pedantic-errors",
+			"-DPLATFORM_WIN32",
+			"-Ilib/"
+		)
 
-	if ($optimize) {
-		$cf += "-O2"
+		if ($optimize) {
+			$cf += "-O2"
+		}
+		if ($debuginfo) {
+			$cf += "-g"
+		}
+
+		$lf = @(
+			"-lopengl32",
+			"-lgdi32",
+			"-luser32"
+			"-lxinput"
+		)
+
+		clang @cf platform_win32.c game.c $engineLibWin @lf -o build/PoolBomb.exe
+		if ($LASTEXITCODE -ne 0) { return }
+
+		rm -Recurse -Force build/assets
+		$null = mkdir build/assets
+		Copy-Item -Path assets/* -Destination build/assets -Recurse -Force
 	}
-	if ($debuginfo) {
-		$cf += "-g"
+
+	if ($web) {
+		$cf = @(
+			"-std=c99",
+			"-Wall",
+			"-Wextra",
+			"-Wno-unused-parameter",
+			"-Wno-unused-function",
+			"-msimd128",
+			"-mbulk-memory",
+			"-Ilib/",
+			"-DPLATFORM_EMSCRIPTEN",
+			"-sINITIAL_MEMORY=16973824"
+		)
+
+		if ($optimize) {
+			$cf += "-O2"
+		}
+		if ($debuginfo) {
+			$cf += "-g"
+		}
+
+		emcc @cf platform_emscripten.c game.c $engineLibWeb --shell-file shell.html --preload-file assets -o build/PoolBomb.html
+		if ($LASTEXITCODE -ne 0) { return }
 	}
-
-	$lf = @(
-		"-lopengl32",
-		"-lgdi32",
-		"-luser32"
-		"-lxinput"
-	)
-
-	clang @cf platform_win32.c game.c $engineLibWin @lf -o build/PoolBomb.exe
-	if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
-
-	rm -Recurse -Force build/assets
-	mkdir build/assets
-	Copy-Item -Path assets/* -Destination build/assets -Recurse -Force
 }
 
-if ($web) {
-	$cf = @(
-		"-std=c99",
-		"-Wall",
-		"-Wextra",
-		"-Wno-unused-parameter",
-		"-Wno-unused-function",
-		"-msimd128",
-		"-mbulk-memory",
-		"-Ilib/",
-		"-DPLATFORM_EMSCRIPTEN",
-		"-sINITIAL_MEMORY=16973824"
-	)
-
-	if ($optimize) {
-		$cf += "-O2"
+function Write-BuildResult {
+	$stamp = Get-Date -Format "HH:mm:ss"
+	if ($LASTEXITCODE -eq 0) {
+		Write-Host "[$stamp] Build succeeded." -ForegroundColor Green
+	} else {
+		Write-Host "[$stamp] Build failed (exit $LASTEXITCODE)." -ForegroundColor Red
 	}
-	if ($debuginfo) {
-		$cf += "-g"
-	}
+}
 
-	emcc @cf platform_emscripten.c game.c $engineLibWeb --shell-file shell.html --preload-file assets -o build/PoolBomb.html
-	if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+
+if ($watch) {
+	Invoke-Build
+	Write-BuildResult
+
+	$watcher = New-Object System.IO.FileSystemWatcher
+	$watcher.Path = (Get-Location).Path
+	$watcher.IncludeSubdirectories = $true
+	$watcher.NotifyFilter = [System.IO.NotifyFilters]::LastWrite -bor [System.IO.NotifyFilters]::FileName
+	$watcher.EnableRaisingEvents = $true
+
+	Write-Host "Watching .c/.h files for changes. Press Ctrl+C to stop." -ForegroundColor Cyan
+	while ($true) {
+		$change = $watcher.WaitForChanged([System.IO.WatcherChangeTypes]::Changed, 500)
+		if ($change.TimedOut) { continue }
+		if ($change.Name -notmatch '\.(c|h)$') { continue }
+
+		$stamp = Get-Date -Format "HH:mm:ss"
+		Write-Host ""
+		Write-Host "[$stamp] $($change.Name) changed, building..." -ForegroundColor Cyan
+		Invoke-Build
+		Write-BuildResult
+	}
+} else {
+	Invoke-Build
+	exit $LASTEXITCODE
 }
--- game.c
+++ game.c
@@ -46,40 +46,29 @@ static void DisplayWalkAnimationSheet(U32 *frame_buffer, Bitmap spriteset, WalkA
 
 static const S32 move_speed = 160+16;
 
-static S64 player_y = 0 * 256;
-static S64 player_x = 0 * 256;
+typedef struct {
+	S32 x, y;
+	Direction animation_direction;
+	U32 animation_tick;
+	U32 animation_index;
+	S32 previous_x_movement, previous_y_movement;
+} Player;
 
-static Direction g_animation_direction = DIRECTION_DOWN;
-static U32 g_animation_tick = 0;
+static void UpdatePlayer(Player *p, GameInput input) {
 
-static S32 y_movement = 0;
-static S32 x_movement = 0;
+	Player player = *p;
 
-static void GameRender(U32 *frame_buffer);
+	Bool down = IsButtonDown(input, BUTTON_DOWN);
+	Bool up = IsButtonDown(input, BUTTON_UP);
+	Bool left = IsButtonDown(input, BUTTON_LEFT);
+	Bool right = IsButtonDown(input, BUTTON_RIGHT);
 
-void GameFrame(U32 *frame_buffer, uint64_t frame_index, GameInput *player_inputs, U32 player_count, Bool should_render) {
-
-	Bool down = False;
-	Bool up = False;
-	Bool left = False;
-	Bool right = False;
-
-	for (U32 i = 0; i < player_count; ++i) {
-		down |= IsButtonDown(player_inputs[i], BUTTON_DOWN);
-		up |= IsButtonDown(player_inputs[i], BUTTON_UP);
-		left |= IsButtonDown(player_inputs[i], BUTTON_LEFT);
-		right |= IsButtonDown(player_inputs[i], BUTTON_RIGHT);
-	}
-
-	S32 previous_x_movement = x_movement;
-	S32 previous_y_movement = y_movement;
-
-	y_movement = (S32)down - (S32)up;
-	x_movement = (S32)right - (S32)left;
+	S32 y_movement = (S32)down - (S32)up;
+	S32 x_movement = (S32)right - (S32)left;
 
 	{
-		S32 player_tile_x = WorldToTile(player_x);
-		S32 player_tile_y = WorldToTile(player_y);
+		S32 player_tile_x = WorldToTile(player.x);
+		S32 player_tile_y = WorldToTile(player.y);
 
 		S32 local_tiles[9];
 		for (S32 y = -1; y <= 1; ++y) {
@@ -101,8 +90,8 @@ void GameFrame(U32 *frame_buffer, uint64_t frame_index, GameInput *player_inputs
 
 		if (y_movement != 0 || x_movement != 0) {
 
-			S32 current_tile_offset_y = (player_y - SUBPIXELS_PER_TILE/2) & (SUBPIXELS_PER_TILE - 1);
-			S32 current_tile_offset_x = (player_x - SUBPIXELS_PER_TILE/2) & (SUBPIXELS_PER_TILE - 1);
+			S32 current_tile_offset_y = (player.y - SUBPIXELS_PER_TILE/2) & (SUBPIXELS_PER_TILE - 1);
+			S32 current_tile_offset_x = (player.x - SUBPIXELS_PER_TILE/2) & (SUBPIXELS_PER_TILE - 1);
 
 			S32 offset_from_center_x = current_tile_offset_x - SUBPIXELS_PER_TILE/2;
 			S32 offset_from_center_y = current_tile_offset_y - SUBPIXELS_PER_TILE/2;
@@ -110,8 +99,8 @@ void GameFrame(U32 *frame_buffer, uint64_t frame_index, GameInput *player_inputs
 			Bool is_in_center_x = offset_from_center_x == 0;
 			Bool is_in_center_y = offset_from_center_y == 0;
 
-			S32 next_player_y = player_y + move_speed*y_movement;
-			S32 next_player_x = player_x + move_speed*x_movement;
+			S32 next_player_y = player.y + move_speed*y_movement;
+			S32 next_player_x = player.x + move_speed*x_movement;
 
 			S32 corner_assist_threshold = SUBPIXELS_PER_TILE*4/16;
 			S32 corner_assist_from_center = SUBPIXELS_PER_TILE/2 - corner_assist_threshold;
@@ -230,34 +219,34 @@ void GameFrame(U32 *frame_buffer, uint64_t frame_index, GameInput *player_inputs
 				}
 
 				if (should_corner_assist_right || continued_corner_assist_right) {
-					player_x += move_speed;
+					player.x += move_speed;
 				} else if (should_corner_assist_left || continued_corner_assist_left) {
-					player_x -= move_speed;
+					player.x -= move_speed;
 				} else {
 
 					if (y_movement > 0) {
 						S32 bottom_edge = TileToWorld(player_tile_y + 1) - 13*256;
 						if (bottom_tile_occupied && next_player_y >= bottom_edge) {
-							player_y = bottom_edge;
+							player.y = bottom_edge;
 						} else {
-							player_y = next_player_y;
+							player.y = next_player_y;
 						}
 					} else {
 						S32 top_edge = TileToWorld(player_tile_y - 1) + 12*256;
 						if (top_tile_occupied && next_player_y <= top_edge) {
-							player_y = top_edge;
+							player.y = top_edge;
 						} else {
-							player_y = next_player_y;
+							player.y = next_player_y;
 						}
 					}
 
 					if (!is_in_center_x) {
 						if (!adjacent_occupied || (y_movement < 0 && offset_from_center_y >= 0) || (y_movement > 0 && offset_from_center_y <= 0)) {
 							if (offset_from_center_x >= -move_speed && offset_from_center_x <= move_speed) {
-								player_x -= offset_from_center_x;
+								player.x -= offset_from_center_x;
 							} else {
 								S32 x_move = (offset_from_center_x >> 31) | 1;
-								player_x += move_speed*(x_move * -1);
+								player.x += move_speed*(x_move * -1);
 							}
 						}
 					}
@@ -280,31 +269,31 @@ void GameFrame(U32 *frame_buffer, uint64_t frame_index, GameInput *player_inputs
 				should_corner_assist_down &= (x_movement > 0 && !bottom_right_occupied) || (x_movement < 0 && !bottom_left_occupied);
 
 				if (should_corner_assist_up) {
-					player_y -= move_speed;
+					player.y -= move_speed;
 				} else if (should_corner_assist_down) {
-					player_y += move_speed;
+					player.y += move_speed;
 				} else if (is_in_center_y || adjacent_occupied) {
 
 					if (x_movement > 0) {
 						Bool right_tile_occupied = local_tiles[1*3 + 2] != 6;
 						S32 right_edge = TileToWorld(player_tile_x + 1) - 15*256;
 						if (right_tile_occupied && next_player_x >= right_edge) {
-							player_x = right_edge;
+							player.x = right_edge;
 						} else {
-							player_x = next_player_x;
+							player.x = next_player_x;
 						}
 					} else {
 						Bool right_tile_occupied = local_tiles[1*3 + 0] != 6;
 						S32 left_edge = TileToWorld(player_tile_x - 1) + 15*256;
 						if (right_tile_occupied && next_player_x <= left_edge) {
-							player_x = left_edge;
+							player.x = left_edge;
 						} else {
-							player_x = next_player_x;
+							player.x = next_player_x;
 						}
 					}
 
 				} else {
-					player_x = next_player_x;
+					player.x = next_player_x;
 
 					Bool cancel_auto_adjust_y = False;
 
@@ -335,10 +324,10 @@ void GameFrame(U32 *frame_buffer, uint64_t frame_index, GameInput *player_inputs
 
 					if (!cancel_auto_adjust_y || force_auto_adjust) {
 						if (offset_from_center_y >= -move_speed && offset_from_center_y <= move_speed) {
-							player_y -= offset_from_center_y;
+							player.y -= offset_from_center_y;
 						} else {
 							S32 y_move = (offset_from_center_y >> 31) | 1;
-							player_y += move_speed*(y_move * -1);
+							player.y += move_speed*(y_move * -1);
 						}
 					}
 				}
@@ -365,24 +354,24 @@ void GameFrame(U32 *frame_buffer, uint64_t frame_index, GameInput *player_inputs
 	}
 
 	if (y_movement != 0 || x_movement != 0) {
-		if (g_animation_direction != next_animation_direction) {
-			g_animation_tick = 0;
+		if (player.animation_direction != next_animation_direction) {
+			player.animation_tick = 0;
 		}
-		g_animation_direction = next_animation_direction;
+		player.animation_direction = next_animation_direction;
 	}
 
 	U32 animation_frame_delay = 4;
 
-	WalkAnimation walk_animation = g_walk_animations[g_animation_direction];
+	WalkAnimation walk_animation = g_walk_animations[player.animation_direction];
 	U32 animation_length = walk_animation.frame_count;
-	Bool started_moving = (x_movement != 0 || y_movement != 0) && (previous_x_movement != 0 && previous_y_movement != 0);
+	Bool started_moving = (x_movement != 0 || y_movement != 0) && (player.previous_x_movement != 0 && player.previous_y_movement != 0);
 	if (started_moving) {
-		g_animation_tick += walk_animation.start_offset * animation_frame_delay;
+		player.animation_tick += walk_animation.start_offset * animation_frame_delay;
 	}
-	U32 animation_index = (g_animation_tick / animation_frame_delay) % animation_length;
+	U32 animation_index = (player.animation_tick / animation_frame_delay) % animation_length;
 	Bool animation_finished = animation_index == 0 || animation_index == walk_animation.neutral_frame;
 	if (x_movement != 0 || y_movement != 0) {
-		g_animation_tick += 1;
+		player.animation_tick += 1;
 	} else if (!animation_finished) {
 		U32 behind = (animation_index <= walk_animation.neutral_frame) ? 0 : walk_animation.neutral_frame;
 		U32 ahead = (animation_index < walk_animation.neutral_frame) ? walk_animation.neutral_frame : animation_length;
@@ -390,18 +379,49 @@ void GameFrame(U32 *frame_buffer, uint64_t frame_index, GameInput *player_inputs
 		Bool should_reverse = (animation_index - behind) <= (ahead - animation_index);
 
 		if (should_reverse) {
-			g_animation_tick -= 1;
-			U32 next_animation_index = (g_animation_tick / animation_frame_delay) % animation_length;
+			player.animation_tick -= 1;
+			U32 next_animation_index = (player.animation_tick / animation_frame_delay) % animation_length;
 			if (next_animation_index == 0) {
-				g_animation_tick = walk_animation.neutral_frame * animation_frame_delay;
+				player.animation_tick = walk_animation.neutral_frame * animation_frame_delay;
 			}
 
 			if (next_animation_index == walk_animation.neutral_frame) {
-				g_animation_tick = 0;
+				player.animation_tick = 0;
 			}
 		} else {
-			g_animation_tick += 1;
+			player.animation_tick += 1;
 		}
+	}
+	player.animation_index = animation_index;
+
+	player.previous_x_movement = x_movement;
+	player.previous_y_movement = y_movement;
+
+
+	*p = player;
+}
+
+void RenderPlayer(Player *player, U32 *frame_buffer, U32 character_index) {
+
+	WalkAnimation walk_animation = g_walk_animations[player->animation_direction];
+	Rectangle sprite_rect = {
+		.x = walk_animation.x_offsets[player->animation_index],
+		.y = 16 + 48*character_index,
+		.width = 32,
+		.height = 32
+	};
+
+	S32 player_render_y = ((player->y - 128)>>8)-8;
+	S32 player_render_x = ((player->x + 128)>>8);
+	BlitBitmapRectangleToFramebuffer(frame_buffer, player_render_x, player_render_y, base_spriteset, sprite_rect);
+}
+
+static Player players[4];
+
+void GameFrame(U32 *frame_buffer, uint64_t frame_index, GameInput *player_inputs, U32 player_count, Bool should_render) {
+
+	for (U32 i = 0; i < player_count; ++i) {
+		UpdatePlayer(&players[i], player_inputs[i]);
 	}
 
 	if (should_render) {
@@ -420,15 +440,27 @@ void GameFrame(U32 *frame_buffer, uint64_t frame_index, GameInput *player_inputs
 			}
 		}
 
-		Rectangle sprite_rect = {
-			.x = walk_animation.x_offsets[animation_index],
-			.y = 16 + 48*0,
-			.width = 32,
-			.height = 32
-		};
+		U32 sorted_indices[4] = {0};
+		for (U32 i = 0; i < player_count; ++i) {
+			sorted_indices[i] = i;
+		}
 
-		S32 player_render_y = ((player_y - 128)>>8)-8;
-		S32 player_render_x = ((player_x + 128)>>8);
-		BlitBitmapRectangleToFramebuffer(frame_buffer, player_render_x, player_render_y, base_spriteset, sprite_rect);
+		for (U32 i = 0; i < player_count; ++i) {
+			for (U32 j = 0; j < player_count; ++j) {
+				Player a = players[sorted_indices[i]];
+				Player b = players[sorted_indices[j]];
+
+				if (b.y > a.y) {
+					U32 temp = sorted_indices[i];
+					sorted_indices[i] = sorted_indices[j];
+					sorted_indices[j] = temp;
+				}
+			}
+		}
+		
+		for (U32 i = 0; i < player_count; ++i) {
+			U32 index = sorted_indices[i];
+			RenderPlayer(&players[index], frame_buffer, index % 2);
+		}
 	}
 }
